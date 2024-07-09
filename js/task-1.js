b710c10a9dc7acaac6f405d952a7cfa302589afe
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2024-07-09 18:02:44 +0200

    task 3 completed

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,4 @@
+console.log("Zadanie 1");
 function slugify(title) {
   return title.toLowerCase().split(" ").join("-");
 }
@@ -6,3 +7,5 @@ console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
 console.log(slugify("English for developer")); // "english-for-developer"
 console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
 console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+console.log("---------------------");
--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,5 @@
+console.log("Zadanie 2");
+
 function makeArray(firstArray, secondArray, maxLength) {
   return firstArray.concat(secondArray).slice(0, maxLength);
 }
@@ -8,3 +10,5 @@ console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); //
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
 console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+
+console.log("---------------------");
